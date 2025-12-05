d0e194c73bc0f9456191e8e45322bb7db8533827
developer 1 <test.platform95@gmail.com>
2025-12-05 20:19:51 +0530

    fix compile errors

--- arka-backend/src/main/java/com/arka/modules/marketplace/config/AmazonBestsellersSeeder.java
+++ arka-backend/src/main/java/com/arka/modules/marketplace/config/AmazonBestsellersSeeder.java
@@ -13,7 +13,6 @@ import java.math.BigDecimal;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
