bd9296628b2a8d97dfeda9fd4a51e2eb14f18598
developer 1 <test.platform95@gmail.com>
2026-03-24 12:55:30 +0530

    offering as a service

--- frontend/src/components/Hero/Hero.tsx
+++ frontend/src/components/Hero/Hero.tsx
@@ -15,7 +15,7 @@ const Hero: React.FC = () => {
       <div className={styles.container}>
         <div className={styles.content}>
           <h1 className={styles.heading}>ARKA</h1>
-          {/* <h2 className={styles.subheading}>Spreading Happiness.</h2> */}
+          <h2 className={styles.subheading}>Offering As A Service.</h2>
           <p className={styles.tagline}>Sustainable Books. Unlimited Possibilities.</p>
 
           <button 
